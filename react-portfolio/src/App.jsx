e06a9224c451ece215a92a7776036fd8fc71397b
Alex <sponisp@gmail.com>
2023-08-02 19:34:41 +0200

    added components and first styling

--- react-portfolio/src/App.jsx
+++ react-portfolio/src/App.jsx
@@ -1,8 +1,27 @@
 import React from 'react'
+import Header from './components/Header'
+import About from './components/About'
+import Nav from './components/Nav'
+import Experience from './components/Experience'
+import Services from './components/Nav'
+import Portfolio from './components/Portfolio'
+import Testimonials from './components/Testimonials'
+import Contact from './components/Contact'
+import Footer from './components/Footer'
 
 const App = () => {
   return (
-    <div>App hello</div>
+    <>
+      <Header />
+      <Nav />
+      <About />
+      <Experience />
+      <Services />
+      <Portfolio />
+      <Testimonials />
+      <Contact />
+      <Footer />
+    </>
   )
 }
 
---
+++ react-portfolio/src/components/Services.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import '../styles/services.css'
+const Services = () => {
+  return (
+    <div>Services</div>
+  )
+}
+
+export default Services
